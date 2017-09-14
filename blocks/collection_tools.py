9ab7c73364922b7e6f16694ae5a1809144ca844f
c6401 <c6401+github@yandex.com>
2017-09-14 20:27:38 +0300

    Collection tools walk_json

---
+++ blocks/collection_tools.py
@@ -0,0 +1,23 @@
+def walk_json(
+    tree,
+    dict_hook=lambda x: x,
+    list_hook=lambda x: x,
+):
+    if tree is None:
+        return tree
+    elif isinstance(tree, (int, float)):
+        return tree
+    elif isinstance(tree, str):
+        return tree
+    elif isinstance(tree, list):
+        return list_hook([
+            walk_json(el, dict_hook=dict_hook)
+            for el
+            in tree
+        ])
+    elif isinstance(tree, dict):
+        return dict_hook({
+            k: walk_json(v, dict_hook=dict_hook)
+            for k, v
+            in tree.items()
+        })
